7b007702c26bb5fc46f56e4474b8627800ef82ed
pjaviuson <pjaviuson@gmail.com>
2026-06-14 15:53:12 +0200

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ function mostrarMensaje() {
   const mensaje = document.getElementById("mensaje");
   mensaje.textContent = "¡GitHub funciona! Has creado tu primera web.";
 }
+function calcularIVA() {
+
+  let precio =
+      parseFloat(
+          document.getElementById("precio").value
+      );
+
+  let total = precio * 1.21;
+
+  document.getElementById("resultado").textContent =
+      "Precio con IVA: " + total.toFixed(2) + " €";
+}
